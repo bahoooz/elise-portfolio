f4d1b4fd05084c538e16c4873ef65e15fa3e74d7
Bahoz <julienbraultcherif@gmail.com>
2023-11-09 23:05:42 +0100

    dernières maj

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ const openBurgerMenu = document.getElementById("open-burger-menu");
 const burgerMenu = document.querySelector(".burger-menu");
 const closeBurgerMenu = document.getElementById("close-burger-menu");
 const bodyApp = document.body;
-const projectSection = document.getElementById("projects");
 const scrollContainer = document.getElementById("scroll-container");
+
+const section1 = document.getElementById("main");
+const section2 = document.getElementById("projects");
+const section3 = document.getElementById("posters");
+const section4 = document.querySelector("footer");
 
 openBurgerMenu.addEventListener("click", () => {
   burgerMenu.style.display = "flex";
+  bodyApp.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  section1.style.background = "rgba(255, 251, 240, 0.5)";
+  section2.style.background = "rgba(255, 251, 240, 0.5)";
+  section3.style.background = "rgba(255, 251, 240, 0.5)";
+  section4.style.background = "rgba(255, 251, 240, 0.5)";
 });
 
 closeBurgerMenu.addEventListener("click", () => {
   burgerMenu.style.display = "none";
+  bodyApp.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  section1.style.background = "rgba(255, 251, 240)";
+  section2.style.background = "rgb(255, 251, 240)";
+  section3.style.background = "rgba(255, 251, 240)";
+  section4.style.background = "rgba(255, 251, 240)";
 });
 
 bodyApp.addEventListener("click", (e) => {
